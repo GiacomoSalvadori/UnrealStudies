ef40b1b361557e95c059cbf5de65a04efb351744
Giacomo Salvadori <giaco.salvadori@gmal.com>
2020-06-19 12:03:09 +0200

    [TECH] Fix on include statement

--- Source/UnrealStudies/ActorThrowable.cpp
+++ Source/UnrealStudies/ActorThrowable.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "HealthComponent.h"
 #include "ActorThrowable.h"
+#include "HealthComponent.h"
 
 // Sets default values
 AActorThrowable::AActorThrowable()
